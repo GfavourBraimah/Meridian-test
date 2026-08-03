46d5b4626774548d6294f9a504bafb84d5496197
God'sfavour Braimah <gfbraimah@gmail.com>
2026-08-03 22:55:46 +0100

    added cicd

--- infra/security.tf
+++ infra/security.tf
@@ -35,7 +35,6 @@ resource "aws_security_group" "meridian_sg" {
     cidr_blocks = ["${chomp(data.http.my_ip.response_body)}/32"]
   }
 
-  # Allow all outbound traffic (so your EC2 can pull Docker images, run apt-get, etc.)
   egress {
     from_port   = 0
     to_port     = 0
