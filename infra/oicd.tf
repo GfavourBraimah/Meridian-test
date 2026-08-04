97fb0aab8a40496b0647dadaac669bc98475fb2d
God'sfavour Braimah <gfbraimah@gmail.com>
2026-08-04 07:46:11 +0100

    updated github name

--- infra/oicd.tf
+++ infra/oicd.tf
@@ -1,10 +1,15 @@
 # oidc.tf
 
-# 1. Register GitHub as an Identity Provider in your AWS IAM
+# Automatically fetch GitHub's latest TLS certificate live!
+data "tls_certificate" "github" {
+  url = "https://token.actions.githubusercontent.com"
+}
+
+# 1. Register GitHub as an Identity Provider using the dynamic thumbprint
 resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"] # GitHub's global certificate thumbprint
+  thumbprint_list = [data.tls_certificate.github.certificates[0].sha1_fingerprint]
 }
 
 # 2. Create an IAM Role that GitHub Actions can assume securely
@@ -22,7 +27,7 @@ resource "aws_iam_role" "github_actions_oidc" {
         Action = "sts:AssumeRoleWithWebIdentity"
         Condition = {
           StringLike = {
-            "token.actions.githubusercontent.com:sub" : "repo:gfavourbraimah/meridian-test:*"
+            "token.actions.githubusercontent.com:sub" : "repo:GfavourBraimah/Meridian-test:*"
           }
           StringEquals = {
             "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com"
@@ -33,7 +38,7 @@ resource "aws_iam_role" "github_actions_oidc" {
   })
 }
 
-# 3. Grant the Role permissions to push to ECR and manage infrastructure if needed
+# 3. Grant the Role permissions to push to ECR
 resource "aws_iam_role_policy_attachment" "ecr_access" {
   role       = aws_iam_role.github_actions_oidc.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
